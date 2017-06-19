d634a7b58e40ffd7b418734419e4591b4426fa1f
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-06-19 15:53:24 +0200

    TO NOT MERGE: fix sql mode to only_full_group_by in behats

--- features/Context/CatalogConfigurationContext.php
+++ features/Context/CatalogConfigurationContext.php
@@ -58,6 +58,11 @@ class CatalogConfigurationContext extends RawMinkContext
         $this->loadCatalog($this->getConfigurationFiles($catalog));
         
         $this->getMainContext()->getContainer()->get('pim_connector.doctrine.cache_clearer')->clear();
+
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $entityManager->getConnection()->exec(
+            'set global sql_mode=\'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION\''
+        );
     }
 
     /**
